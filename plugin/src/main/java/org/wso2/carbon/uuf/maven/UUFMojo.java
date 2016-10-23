ae5a21a80646fd8664a01cbddd1830fed4c97d08
Sajith Janaprasad <sajithar@wso2.com>
2016-10-23 08:25:52 +0530

    added UUFMojo interface and AbstractZipMojo class

---
+++ plugin/src/main/java/org/wso2/carbon/uuf/maven/UUFMojo.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.uuf.maven;
+
+import org.apache.maven.plugin.Mojo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public interface UUFMojo extends Mojo {
+
+    String ARTIFACT_TYPE_UUF_APP = "carbon-feature";
+    String ARTIFACT_TYPE_UUF_COMPONENT = "uuf-component";
+    String ARTIFACT_TYPE_UUF_THEME = "uuf-theme";
+
+    String DIRECTORY_COMPONENTS = "/components";
+    String DIRECTORY_THEMES = "/themes";
+
+    String EXPRESSION_SOURCE_DIRECTORY_PATH = "${project.basedir}/src/main/";
+    String EXPRESSION_CONFIGURATION_INSTRUCTIONS = "instructions";
+
+    String CONFIGURATION_IMPORT_PACKAGE = "Import-Package";
+
+    List<String> EXCLUDING_FILE_PATTERNS = new ArrayList<>(Arrays.asList("**/*.iml", "**/*.ipr", "**/*.iwr",
+                                                                         "**/*.eclipse", "**/target/**", "**/pom.xml",
+                                                                         "**/assembly.xml"));
+}
