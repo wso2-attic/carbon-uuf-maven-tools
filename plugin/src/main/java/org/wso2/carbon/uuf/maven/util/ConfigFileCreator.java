98f67f00d6c6614f989f3373474fefd202629cee
Sajith Janaprasad <sajithar@wso2.com>
2016-11-23 11:19:13 +0530

    added 'createConfigYaml', 'createDependencyTree' static methods to ConfigFileCreator class

--- plugin/src/main/java/org/wso2/carbon/uuf/maven/util/ConfigFileCreator.java
+++ plugin/src/main/java/org/wso2/carbon/uuf/maven/util/ConfigFileCreator.java
@@ -27,6 +27,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -38,47 +39,72 @@ import java.util.stream.Collectors;
 public class ConfigFileCreator {
 
     private static final String FILE_OSGI_IMPORTS = "osgi-imports";
+    private static final String FILE_CONFIG_YAML = "config.yaml";
+    private static final String FILE_DEPENDENCY_TREE = "dependency-tree.yaml";
     private static final String FILE_FEATURE_PROPERTIES = "feature.properties";
     private static final String FILE_P2_INF = "p2.inf";
 
-    public static void createOsgiImports(String osgiImportsConfig, Path outputDirectory) throws MojoExecutionException {
-        if ((osgiImportsConfig == null) || osgiImportsConfig.isEmpty()) {
+    public static void createOsgiImports(String osgiImportsContent, String outputDirectoryPath)
+            throws MojoExecutionException {
+        if ((osgiImportsContent == null) || osgiImportsContent.isEmpty()) {
             return;
         }
 
-        Path osgiImportsFile = outputDirectory.resolve(FILE_OSGI_IMPORTS);
         try {
-            createDirectory(outputDirectory);
-            String osgiImports = Arrays.stream(osgiImportsConfig.trim().split(","))
+            String osgiImports = Arrays.stream(osgiImportsContent.trim().split(","))
                     .map(String::trim)
                     .filter(s -> !s.isEmpty())
                     .collect(Collectors.joining("\n"));
             String content = applyTemplate(FILE_OSGI_IMPORTS, osgiImports);
-            writeFile(osgiImportsFile, content);
+            writeFile(Paths.get(outputDirectoryPath, FILE_OSGI_IMPORTS), content);
         } catch (IOException e) {
-            throw new MojoExecutionException("Cannot create file '" + osgiImportsFile + "'. " + e.getMessage(), e);
+            throw new MojoExecutionException(
+                    "Cannot create '" + FILE_OSGI_IMPORTS + "' file in '" + outputDirectoryPath + "'. " +
+                            e.getMessage(), e);
         }
     }
 
-    public static Path createFeatureProperties(Path outputDirectory) throws MojoExecutionException {
+    public static void createConfigYaml(String configYamlContent, String outputDirectoryPath)
+            throws MojoExecutionException {
+        try {
+            writeFile(Paths.get(outputDirectoryPath, FILE_CONFIG_YAML), configYamlContent);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Cannot create '" + FILE_CONFIG_YAML + "' file in '" + outputDirectoryPath
+                                                     + "'. " + e.getMessage(), e);
+        }
+    }
+
+    public static void createDependencyTree(String dependencyTreeContent, String outputDirectoryPath)
+            throws MojoExecutionException {
+        try {
+            writeFile(Paths.get(outputDirectoryPath, FILE_DEPENDENCY_TREE), dependencyTreeContent);
+        } catch (IOException e) {
+            throw new MojoExecutionException(
+                    "Cannot create '" + FILE_DEPENDENCY_TREE + "' file in '" + outputDirectoryPath + "'. " +
+                            e.getMessage(), e);
+        }
+    }
+
+    public static String createFeatureProperties(String outputDirectoryPath) throws MojoExecutionException {
+        Path outputDirectory = Paths.get(outputDirectoryPath);
         Path buildPropertiesFile = outputDirectory.resolve(FILE_FEATURE_PROPERTIES);
         try {
             createDirectory(outputDirectory);
             String content = readTemplate(FILE_FEATURE_PROPERTIES);
             writeFile(buildPropertiesFile, content);
-            return buildPropertiesFile;
+            return buildPropertiesFile.toString();
         } catch (IOException e) {
             throw new MojoExecutionException("Cannot create file '" + buildPropertiesFile + "'. " + e.getMessage(), e);
         }
     }
 
-    public static Path createP2Inf(String featureName, Path outputDirectory) throws MojoExecutionException {
+    public static void createP2Inf(String featureName, String outputDirectoryPath) throws MojoExecutionException {
+        Path outputDirectory = Paths.get(outputDirectoryPath);
         Path p2InfFile = outputDirectory.resolve(FILE_P2_INF);
         try {
             createDirectory(outputDirectory);
             String content = applyTemplate(FILE_P2_INF, featureName, featureName);
             writeFile(p2InfFile, content);
-            return p2InfFile;
         } catch (IOException e) {
             throw new MojoExecutionException("Cannot create file '" + p2InfFile + "'. " + e.getMessage(), e);
         }
