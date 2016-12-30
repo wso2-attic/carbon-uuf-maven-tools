0d21a06d2a28c93d2f973aa6d6ea117763f1ec4a
Sajith Janaprasad <sajithar@wso2.com>
2016-12-30 07:56:36 +0530

    added test cases for AppConfig bean class

---
+++ plugin/src/test/java/org/wso2/carbon/uuf/maven/bean/AppConfigTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.uuf.maven.bean;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+
+/**
+ * Test cases for app's config bean.
+ */
+public class AppConfigTest {
+
+    @Test
+    public void testContextPathValidations() {
+        AppConfig appConfig = createAppConfig();
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setContextPath(""));
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setContextPath("abc"));
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setContextPath("a/bc"));
+
+        appConfig.setContextPath(null);
+        appConfig.setContextPath("/abc");
+    }
+
+    @Test
+    public void testThemeValidations() {
+        AppConfig appConfig = createAppConfig();
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setTheme(""));
+
+        appConfig.setTheme(null);
+        appConfig.setTheme("org.wso2.carbon.uuf.sample.theme.default");
+    }
+
+    @Test
+    public void testLoginPageUriValidations() {
+        AppConfig appConfig = createAppConfig();
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setLoginPageUri(""));
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setLoginPageUri("login"));
+        Assert.assertThrows(IllegalArgumentException.class, () -> appConfig.setLoginPageUri("l/ogin"));
+
+        appConfig.setLoginPageUri(null);
+        appConfig.setLoginPageUri("/sso/login");
+    }
+
+    @Test
+    public void testErrorPagesValidations() {
+        AppConfig appConfig = createAppConfig();
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> appConfig.setErrorPages(Collections.singletonMap("abc", "/foundation/error/404")));
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> appConfig.setErrorPages(Collections.singletonMap("99", "/foundation/error/404")));
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> appConfig.setErrorPages(Collections.singletonMap("1000", "/foundation/error/404")));
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> appConfig.setErrorPages(Collections.singletonMap("404", "")));
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> appConfig.setErrorPages(Collections.singletonMap("404", "foundation/error/404")));
+
+        appConfig.setErrorPages(null);
+        appConfig.setErrorPages(Collections.emptyMap());
+        appConfig.setErrorPages(Collections.singletonMap("404", "/foundation/error/404"));
+        appConfig.setErrorPages(Collections.singletonMap("default", "/foundation/error/default"));
+    }
+
+    @Test
+    public void testSecurityConfigValidations() {
+        AppConfig.PatternsConfig patternsConfig = new AppConfig.PatternsConfig();
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> patternsConfig.setAccept(Collections.singletonList("")));
+        Assert.assertThrows(IllegalArgumentException.class,
+                            () -> patternsConfig.setReject(Collections.singletonList("")));
+
+        patternsConfig.setAccept(null);
+        patternsConfig.setAccept(Collections.emptyList());
+        patternsConfig.setAccept(Collections.singletonList("/some/pattern"));
+        patternsConfig.setReject(null);
+        patternsConfig.setReject(Collections.emptyList());
+        patternsConfig.setReject(Collections.singletonList("/some/pattern"));
+    }
+
+    private static AppConfig createAppConfig() {
+        return new AppConfig();
+    }
+}
